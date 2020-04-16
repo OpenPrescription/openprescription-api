bd13043776da6ef31bda140a3ca6ba49643c6542
Edilson Osorio Jr <osoriojr@gmail.com>
2020-04-17 02:19:38 +0300

    Added console.log(i18n.getLocale())) (#14)

--- src/mails/patient-prescription/index.js
+++ src/mails/patient-prescription/index.js
@@ -1,8 +1,11 @@
 import Mail from "../../tools/mail";
+import i18n from "i18n";
+
 
 export default class PatientPrescriptionMail extends Mail {
   constructor(context) {
     super(context);
     this.template = "prescription";
+    console.log("LOCALE EMAIL: " + i18n.getLocale());
   }
 }
