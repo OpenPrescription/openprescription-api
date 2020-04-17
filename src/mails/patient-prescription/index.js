a56611dffd8e30af90b53e6bde06131b8f1d085d
Edilson Osorio Jr <osoriojr@gmail.com>
2020-04-17 14:42:48 +0300

    Changing trarnslating mode for emails

--- src/mails/patient-prescription/index.js
+++ src/mails/patient-prescription/index.js
@@ -1,8 +1,12 @@
 import Mail from "../../tools/mail";
+import i18n from "i18n";
+
 
 export default class PatientPrescriptionMail extends Mail {
   constructor(context) {
     super(context);
-    this.template = __("prescription_mail_template");
+    //this.template = "prescription";
+    console.log(i18n.__("prescription_mail_template"));
+    this.template = i18n.__("prescription_mail_template");
   }
 }
