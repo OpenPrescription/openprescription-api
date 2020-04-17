c1af49eaa8ebd82a3cfbdeb1c57d253a206dbebf
Edilson Osorio Jr <osoriojr@gmail.com>
2020-04-17 14:51:36 +0300

    removed all updates

--- src/mails/patient-prescription/index.js
+++ src/mails/patient-prescription/index.js
@@ -1,12 +1,8 @@
 import Mail from "../../tools/mail";
-import i18n from "i18n";
-
 
 export default class PatientPrescriptionMail extends Mail {
   constructor(context) {
     super(context);
-    //this.template = "prescription";
-    console.log(i18n.__("prescription_mail_template"));
-    this.template = i18n.__("prescription_mail_template");
+    this.template = "prescription";
   }
 }
