36617bd60d8c5e858bb41a8b72d322ba6fef1058
Edilson Osorio Jr <osoriojr@gmail.com>
2020-04-17 14:43:57 +0300

    Hotfix locale logandmethod (#17)
    * Added console.log(i18n.getLocale()))
    
    * Log mail and cert  translation lines
    
    * Removed logs
    Changed mail template depending on language
    
    * Changing trarnslating mode for emails

--- src/mails/patient-prescription/index.js
+++ src/mails/patient-prescription/index.js
@@ -5,6 +5,8 @@ import i18n from "i18n";
 export default class PatientPrescriptionMail extends Mail {
   constructor(context) {
     super(context);
-    this.template = __("prescription_mail_template");
+    //this.template = "prescription";
+    console.log(i18n.__("prescription_mail_template"));
+    this.template = i18n.__("prescription_mail_template");
   }
 }
