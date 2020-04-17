281980486e63657698745b3291844dda59c18bf8
Edilson Osorio Jr <osoriojr@gmail.com>
2020-04-17 14:20:43 +0300

    Hotfix addlog locale (#16)
    * Removed logs
    Changed mail template depending on language

--- src/mails/patient-prescription/index.js
+++ src/mails/patient-prescription/index.js
@@ -5,7 +5,6 @@ import i18n from "i18n";
 export default class PatientPrescriptionMail extends Mail {
   constructor(context) {
     super(context);
-    this.template = "prescription";
-    console.log("LOCALE EMAIL: " + i18n.getLocale());
+    this.template = __("prescription_mail_template");
   }
 }
