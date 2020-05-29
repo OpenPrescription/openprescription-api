999c750ce800cd9a00b62d6ecf355011c9777f15
devdigitalrepublic <dev@digitalrepublic.com.br>
2020-05-29 11:33:15 -0300

    Normalize doctor names (#26)
    Co-authored-by: Uriel Miranda <miranda.uriel@gmail.com>

--- src/tools/medical-validator/brazilian-federal-council-of-medicine/index.js
+++ src/tools/medical-validator/brazilian-federal-council-of-medicine/index.js
@@ -3,7 +3,6 @@ import DoctorValidation from "../interfaces/doctor-validation.interface";
 import AwsAPIGateway from "../../aws-api-gateway";
 
 export default class BrazilianFederalCouncilofMedicine {
-
   constructor(doctor) {
     this.doctor = doctor;
     this.name = "cfm";
@@ -17,11 +16,15 @@ export default class BrazilianFederalCouncilofMedicine {
     return doctorName.split(" ")[0];
   }
 
+  _normalizeName(text) {
+    return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  }
+
   async _request() {
     console.log(this.doctor);
-    const response = await new AwsAPIGateway('kvglzg9apl').get('/search/br', {
+    const response = await new AwsAPIGateway("kvglzg9apl").get("/search/br", {
       doctorid: this._removeStateFromDoctorId(this.doctor.id),
-      doctorname: this._getFirstName(this.doctor.name),
+      doctorname: this._normalizeName(this._getFirstName(this.doctor.name)),
     });
     return response.data;
   }
